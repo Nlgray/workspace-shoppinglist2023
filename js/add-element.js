bce22e48177136e5c65b4a7ec097f8a301696dc6
Nlgray <Natdog02@icloud.com>
2023-11-20 12:41:53 -0500

    ..

--- js/add-element.js
+++ js/add-element.js
@@ -5,29 +5,51 @@
 // get all the hot class elements- change them to cool class
 
 // traverse the elements
+$('#two').next().next().text("candy");
 
 // add a new element by clicking the plus sign
+$('#add').click(addElement);
 
 // before and after are for siblings
 // append and prepend are for parent
 
 function addElement() {
+
+
+  $("$todo").append("<li><input type=\"text\"><li>");
   // add a new element
   // add a input text box
   
   // whenever the user are done add the element
+  $('input').blur(function(){
+    var userinput = $(this).val();
+    $(this).parent().addclass("cool");
+    $(this).parent().text(userinput);
+  
+ } );
+ $('li').click(changeStyle);
   
 }
 
 // bind click with the event handler
+$('li').click(changeStyle);
 
 //  click the li element will change the changeStyle
 //  three style : complete, cool, hot
-function changeStyle() {}
+function changeStyle() {
+  if ($(this).hasClass('cool')){
+    $(this).removeclass('cool')
+  }else{
+$(this).addElement('warm')
+  }
+
+
+}
 
 // delete complete element by clicking the trash can
 document.getElementById('remove').addEventListener('click', removeElement);
 
 function removeElement() {
   // remove the marked elements  -- element with style complete
+  $('li.complete').remove();
 }
