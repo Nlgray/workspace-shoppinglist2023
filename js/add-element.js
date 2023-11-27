8755b1e8ed8b9813d9e3f9ac3faf33a41175eeb1
Nlgray <Natdog02@icloud.com>
2023-11-27 12:45:48 -0500

    11/27/23

--- js/add-element.js
+++ js/add-element.js
@@ -1,48 +1,47 @@
 /*
  * Practice on Elements
  */
+const myarray = [1,2,3,4,5];
+myarray.push(10);
+console.log(myarray);
 
 // get all the hot class elements- change them to cool class
+$(".hot").each(function(){
+  //this.setAttribute("class", "cool");
+  $(this).removeClass("hot");
+  // add the class cool
+  $(this).addClass("cool");
+});
 
 // traverse the elements
-$('#two').next().next().text("candy");
+$("#two").next().next().text("Candy");
+
 
 // add a new element by clicking the plus sign
-$('#add').click(addElement);
-
+$("#add").click(addElement);
 // before and after are for siblings
 // append and prepend are for parent
 
 function addElement() {
-
-
-  $("$todo").append("<li><input type=\"text\"><li>");
   // add a new element
   // add a input text box
-  
+  $("#todo").append("<li><input type=\"text\"></li>"); 
   // whenever the user are done add the element
   $('input').blur(function(){
-    var userinput = $(this).val();
-    $(this).parent().addclass("cool");
-    $(this).parent().text(userinput);
-  
- } );
- $('li').click(changeStyle);
-  
+      var userinput = $(this).val();
+      $(this).parent().addClass("cool");
+      $(this).parent().text(userinput);
+  });
+  $('li').click(changeStyle);
 }
 
 // bind click with the event handler
 $('li').click(changeStyle);
-
 //  click the li element will change the changeStyle
 //  three style : complete, cool, hot
 function changeStyle() {
-  if ($(this).hasClass('cool')){
-    $(this).removeclass('cool')
-  }else{
-$(this).addElement('warm')
-  }
-
+  //if ($(this).hasClass('cool'))
+  // finish it please
 
 }
 
